aa2cc2aa7d0d964ae1b99c14fca000e40b7c76af
Manko-C <csalamartin2@gmail.com>
2024-10-11 11:21:29 +0200

    css kell

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-function valasz(){
-    let valasz = document.getElementsByClassName('valasz')
+function valasz(elem) {
+
+    let answers = {
+        hun: "Budapest",
+        can: "Ottawa",
+        jap: "Tokió",
+        ger: "Berlin",
+        mex: "Mexikóváros",
+        per: "Lima"
+    };
+    document.getElementById(elem).innerText = answers[elem];
 }
+
